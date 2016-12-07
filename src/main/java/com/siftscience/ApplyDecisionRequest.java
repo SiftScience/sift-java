74604ba6667e96d6b607db77e74ca486b60b0d83
Arjun Krishnaiah <arjunk@siftscience.com>
2016-12-07 12:07:55 -0800

    refactor apply decision fieldset, tests

--- src/main/java/com/siftscience/ApplyDecisionRequest.java
+++ src/main/java/com/siftscience/ApplyDecisionRequest.java
@@ -7,17 +7,26 @@ import okhttp3.*;
 
 public class ApplyDecisionRequest extends SiftRequest<ApplyDecisionResponse>{
 
-    private String apiKey;
-
-    ApplyDecisionRequest(HttpUrl baseUrl, OkHttpClient okClient, ApplyDecisionFieldSet fields, String apiKey) {
+    ApplyDecisionRequest(HttpUrl baseUrl, OkHttpClient okClient, ApplyDecisionFieldSet fields) {
         super(baseUrl, okClient, fields);
-        this.apiKey = apiKey;
     }
 
     @Override
     protected HttpUrl path(HttpUrl baseUrl) {
-        return baseUrl;
+        HttpUrl.Builder path = baseUrl.newBuilder("v3/accounts")
+                .addPathSegment(((ApplyDecisionFieldSet) fieldSet).getAccountId())
+                .addPathSegment("users")
+                .addPathSegment(((ApplyDecisionFieldSet) fieldSet).getUserId());
+
+        String orderId = ((ApplyDecisionFieldSet) fieldSet).getOrderId();
+        if (orderId != null && !orderId.isEmpty()) {
+            path.addPathSegment("orders").addPathSegment(orderId);
+        }
+
+        return path.addPathSegment("decisions").build();
     }
+
+
 
     @Override
     ApplyDecisionResponse buildResponse(okhttp3.Response response, FieldSet requestFields) throws IOException {
@@ -27,7 +36,7 @@ public class ApplyDecisionRequest extends SiftRequest<ApplyDecisionResponse>{
     @Override
     protected void modifyRequestBuilder(Request.Builder builder) {
         super.modifyRequestBuilder(builder);
-        builder.header("Authorization", Credentials.basic(apiKey,""));
+        builder.header("Authorization", Credentials.basic(fieldSet.getApiKey(),""));
     }
 
 }
--- src/main/java/com/siftscience/model/ApplyDecisionFieldSet.java
+++ src/main/java/com/siftscience/model/ApplyDecisionFieldSet.java
@@ -1,20 +1,57 @@
 package com.siftscience.model;
 
+import com.google.gson.*;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import com.siftscience.FieldSet;
 import com.siftscience.exception.MissingFieldException;
 
+import java.lang.reflect.Type;
+
 public class ApplyDecisionFieldSet extends FieldSet<ApplyDecisionFieldSet> {
 
     public enum DecisionSource {
-        MANUAL_REVIEW, AUTOMATED_RULE, CHARGEBACK
+        MANUAL_REVIEW("manual_review"),
+        AUTOMATED_RULE("automated_rule"),
+        CHARGEBACK("chargeback");
+
+        private final String value;
+
+        DecisionSource(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString(){
+            return value;
+        }
+
+        static class DecisionSourceDeserializer implements JsonDeserializer<DecisionSource> {
+            @Override
+            public DecisionSource deserialize(JsonElement json,
+                                              Type typeOfT,
+                                              JsonDeserializationContext context)
+                    throws JsonParseException {
+                return DecisionSource.valueOf(json.getAsString().toUpperCase());
+            }
+        }
+
+        static class DecisionSourceSerializer implements JsonSerializer<DecisionSource> {
+            @Override
+            public JsonElement serialize(DecisionSource source, Type typeOfSrc, JsonSerializationContext context) {
+                return gson.toJsonTree(source.value);
+            }
+        }
     }
+
 
     @Expose @SerializedName("decision_id") private String decisionId;
     @Expose @SerializedName("source") private DecisionSource source;
     @Expose @SerializedName("analyst") private String analyst;
     @Expose @SerializedName("time") private long time;
+    private String accountId;
+    private String userId;
+    private String orderId;
 
     public ApplyDecisionFieldSet() {}
 
@@ -36,6 +73,33 @@ public class ApplyDecisionFieldSet extends FieldSet<ApplyDecisionFieldSet> {
     public ApplyDecisionFieldSet setTime(long time) {
         this.time = time;
         return this;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public ApplyDecisionFieldSet setAccountId(String accountId) {
+        this.accountId = accountId;
+        return this;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public ApplyDecisionFieldSet setUserId(String userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public ApplyDecisionFieldSet setOrderId(String orderId) {
+        this.orderId = orderId;
+            return this;
     }
 
     @Override
