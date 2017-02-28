97624a575cb62a06d7969bad8c96250fa909b761
Alex Paino <alexp@siftscience.com>
2017-02-28 13:08:34 -0800

    Add support for verification, app, and browser

---
+++ src/main/java/com/siftscience/model/App.java
@@ -0,0 +1,77 @@
+package com.siftscience.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class App {
+    @Expose @SerializedName("$os") private String operatingSystem;
+    @Expose @SerializedName("$os_version") private String operatingSystemVersion;
+    @Expose @SerializedName("$device_manufacturer") private String deviceManufacturer;
+    @Expose @SerializedName("$device_model") private String deviceModel;
+    @Expose @SerializedName("$device_unique_id") private String deviceUniqueId;
+    @Expose @SerializedName("$app_name") private String appName;
+    @Expose @SerializedName("$app_version") private String appVersion;
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public App setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+        return this;
+    }
+
+    public String getOperatingSystemVersion() {
+        return operatingSystemVersion;
+    }
+
+    public App setOperatingSystemVersion(String operatingSystemVersion) {
+        this.operatingSystemVersion = operatingSystemVersion;
+        return this;
+    }
+
+    public String getDeviceManufacturer() {
+        return deviceManufacturer;
+    }
+
+    public App setDeviceManufacturer(String deviceManufacturer) {
+        this.deviceManufacturer = deviceManufacturer;
+        return this;
+    }
+
+    public String getDeviceModel() {
+        return deviceModel;
+    }
+
+    public App setDeviceModel(String deviceModel) {
+        this.deviceModel = deviceModel;
+        return this;
+    }
+
+    public String getDeviceUniqueId() {
+        return deviceUniqueId;
+    }
+
+    public App setDeviceUniqueId(String deviceUniqueId) {
+        this.deviceUniqueId = deviceUniqueId;
+        return this;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public App setAppName(String appName) {
+        this.appName = appName;
+        return this;
+    }
+
+    public String getAppVersion() {
+        return appVersion;
+    }
+
+    public App setAppVersion(String appVersion) {
+        this.appVersion = appVersion;
+        return this;
+    }
+}
--- src/test/java/com/siftscience/LoginEventTest.java
+++ src/test/java/com/siftscience/LoginEventTest.java
@@ -1,5 +1,7 @@
 package com.siftscience;
 
+import com.siftscience.model.App;
+import com.siftscience.model.Browser;
 import com.siftscience.model.LoginFieldSet;
 import okhttp3.HttpUrl;
 import okhttp3.mockwebserver.MockResponse;
@@ -12,13 +14,21 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import static java.net.HttpURLConnection.HTTP_OK;
 
 public class LoginEventTest {
+
     @Test
-    public void testLogin() throws Exception {
+    public void testLoginWithApp() throws Exception {
+        String operatingSystem = "iOS";
+        String appName = "Calculator";
+
         String expectedRequestBody = "{\n" +
                 "  \"$type\"         : \"$login\",\n" +
                 "  \"$api_key\"      : \"your_api_key_here\",\n" +
                 "  \"$user_id\"      : \"billy_jones_301\",\n" +
-                "  \"$login_status\" : \"$success\"\n" +
+                "  \"$login_status\" : \"$success\",\n" +
+                "  \"$app\"          : {\n" +
+                "      \"$os\"       : \"" + operatingSystem + "\",\n" +
+                "      \"$app_name\" : \"" + appName + "\"\n" +
+                "  }\n" +
                 "}";
 
         // Start a new mock server and enqueue a mock response.
@@ -42,7 +52,67 @@ public class LoginEventTest {
         // Build and execute the request against the mock server.
         SiftRequest request = client.buildRequest(new LoginFieldSet()
                 .setUserId("billy_jones_301")
-                .setLoginStatus("$success"));
+                .setLoginStatus("$success")
+                .setApp(new App()
+                        .setOperatingSystem(operatingSystem)
+                        .setAppName(appName)));
+
+        SiftResponse siftResponse = request.send();
+
+        // Verify the request.
+        RecordedRequest request1 = server.takeRequest();
+        Assert.assertEquals("POST", request1.getMethod());
+        Assert.assertEquals("/v204/events", request1.getPath());
+        JSONAssert.assertEquals(expectedRequestBody, request.getFieldSet().toJson(), true);
+
+        // Verify the response.
+        Assert.assertEquals(HTTP_OK, siftResponse.getHttpStatusCode());
+        Assert.assertEquals(0, (int) siftResponse.getBody().getStatus());
+        JSONAssert.assertEquals(response.getBody().readUtf8(),
+                siftResponse.getBody().toJson(), true);
+
+        server.shutdown();
+
+    }
+
+    @Test
+    public void testLoginWithBrowswer() throws Exception {
+        String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3)";
+
+        String expectedRequestBody = "{\n" +
+                "  \"$type\"         : \"$login\",\n" +
+                "  \"$api_key\"      : \"your_api_key_here\",\n" +
+                "  \"$user_id\"      : \"billy_jones_301\",\n" +
+                "  \"$login_status\" : \"$success\",\n" +
+                "  \"$browser\"          : {\n" +
+                "      \"$user_agent\"       : \"" + userAgent + "\"\n" +
+                "  }\n" +
+                "}";
+
+        // Start a new mock server and enqueue a mock response.
+        MockWebServer server = new MockWebServer();
+        MockResponse response = new MockResponse();
+        response.setResponseCode(HTTP_OK);
+        response.setBody("{\n" +
+                "    \"status\" : 0,\n" +
+                "    \"error_message\" : \"OK\",\n" +
+                "    \"time\" : 1327604222,\n" +
+                "    \"request\" : \"" + TestUtils.unescapeJson(expectedRequestBody) + "\"\n" +
+                "}");
+        server.enqueue(response);
+        server.start();
+        HttpUrl baseUrl = server.url("");
+
+        // Create a new client and link it to the mock server.
+        SiftClient client = new SiftClient("your_api_key_here");
+        client.setBaseUrl(baseUrl);
+
+        // Build and execute the request against the mock server.
+        SiftRequest request = client.buildRequest(new LoginFieldSet()
+                .setUserId("billy_jones_301")
+                .setLoginStatus("$success")
+                .setBrowser(new Browser()
+                        .setUserAgent(userAgent)));
 
         SiftResponse siftResponse = request.send();
 
